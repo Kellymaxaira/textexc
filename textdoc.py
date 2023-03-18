2a87c9440579c5329cad88e095de628a63886980
Sykamiotis Aggelos <aggelossik@lizardhost.gr>
2023-03-18 13:47:03 +0200

    new api feature

--- textdoc.py
+++ textdoc.py
@@ -16,3 +16,93 @@ b = summing(8)
 
 print(b)
 
+def summing(a):
+    a = a + 4
+    return a
+
+
+b = summing(8)
+
+print(b)
+
+def summing(a):
+    a = a + 4
+    return a
+
+
+b = summing(8)
+
+print(b)
+
+def summing(a):
+    a = a + 4
+    return a
+
+
+b = summing(8)
+
+print(b)
+
+def summing(a):
+    a = a + 4
+    return a
+
+
+b = summing(8)
+
+print(b)
+
+def summing(a):
+    a = a + 4
+    return a
+
+
+b = summing(8)
+
+print(b)
+
+def summing(a):
+    a = a + 4
+    return a
+
+
+b = summing(8)
+
+print(b)
+
+def summing(a):
+    a = a + 4
+    return a
+
+
+b = summing(8)
+
+print(b)
+
+def summing(a):
+    a = a + 4
+    return a
+
+
+b = summing(8)
+
+print(b)
+
+def summing(a):
+    a = a + 4
+    return a
+
+
+b = summing(8)
+
+print(b)
+
+def summing(a):
+    a = a + 4
+    return a
+
+
+b = summing(8)
+
+print(b)
+
